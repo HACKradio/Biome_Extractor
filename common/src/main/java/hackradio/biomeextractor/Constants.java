f0fef2fb0c0f65a1c9ed616fef1f3a7cb116fc1e
HACKradio <hackradio@gmail.com>
2026-05-03 01:14:20 -0700

    Initial commit

---
+++ common/src/main/java/hackradio/biomeextractor/Constants.java
@@ -0,0 +1,11 @@
+package hackradio.biomeextractor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "biomeextractor";
+	public static final String MOD_NAME = "Biome Extractor";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
